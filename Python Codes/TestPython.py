a78dc8152115187dcc2331c7fd95bbb4ef50842f
Sweta Rani <swetasr1509@gmail.com>
2021-01-11 20:33:26 +0530

    first commit

---
+++ Python Codes/TestPython.py
@@ -0,0 +1,11 @@
+mon_sales = "121"
+tues_sales = "105"
+wed_sales = "110"
+thurs_sales = "98"
+fri_sales = "95"
+
+#TODO: Print a string with this format: This week's total sales: xxx
+# You will probably need to write some lines of code before the print statement.
+total_sales = (float(mon_sales) + float(tues_sales) + float(wed_sales) 
+                + float(thurs_sales) + float(fri_sales))
+print("This week\'s total sales: " + str(total_sales))
